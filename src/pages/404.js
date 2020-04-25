99992e5b0c57a8a6e5c48688e48b36fc7fef51d2
Kurt Kemple <kurtkemple@kurtsapollombp.lan>
2020-04-25 13:31:51 -0400

    add tags for 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,7 +7,7 @@ class NotFoundPage extends React.Component {
   render() {
     return (
       <Layout location={this.props.location}>
-        <SEO title="404: Not Found" />
+        <SEO title="404 Not Found" tags={['not found', '404', 'lost']} />
         <h1>Not Found</h1>
         <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
       </Layout>
