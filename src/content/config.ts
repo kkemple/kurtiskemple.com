b41a0ab579c6bb9647e18bd1b5a3c5a7e7e2cadb
Kurtis Kemple <kurtiskemple@gmail.com>
2025-07-21 22:10:34 -0400

    remove double divider

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,18 +1,18 @@
 // 1. Import utilities from `astro:content`
-import { z, defineCollection } from 'astro:content';
+import { z, defineCollection } from "astro:content";
 // 2. Define your collection(s)
 const blogCollection = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    pubDate: z.string(),
-    image: z.string().optional(),
-    featured: z.boolean().optional(),
-  })
- });
+	type: "content",
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		pubDate: z.string(),
+		image: z.string().optional(),
+		featured: z.boolean().optional(),
+	}),
+});
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
-  'blog': blogCollection,
+	blog: blogCollection,
 };
