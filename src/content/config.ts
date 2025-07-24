91ab053270116db3702e98fc2d7ea9a6fbd45792
Kurtis Kemple <kurtiskemple@gmail.com>
2025-07-24 14:28:30 -0400

    add field guide and new site section

--- src/content/config.ts
+++ src/content/config.ts
@@ -11,8 +11,22 @@ const blogCollection = defineCollection({
 		featured: z.boolean().optional(),
 	}),
 });
+
+const informationPhysicsCollection = defineCollection({
+	type: "content",
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		pubDate: z.string(),
+		image: z.string().optional(),
+		featured: z.boolean().optional(),
+		slug: z.string().optional(),
+	}),
+});
+
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
 	blog: blogCollection,
+	"information-physics": informationPhysicsCollection,
 };
